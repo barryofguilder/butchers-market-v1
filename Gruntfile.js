adee57b235981ee2d3c5a147a00d81a8533203ab
Jason Barry <barryofguilder@gmail.com>
2015-01-25 18:10:10 -0500

    Install Grunt and Bower.

---
+++ Gruntfile.js
@@ -0,0 +1,198 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    'cache-busting': {
+      options: {
+        cleanup: true
+      },
+      css: {
+        replace: ['build/**/*.html'],
+        replacement: 'bm.css',
+        file: 'build/css/bm.css'
+      },
+      scriptsApp: {
+        replace: ['build/**/*.html'],
+        replacement: 'bm.js',
+        file: 'build/js/bm.js'
+      },
+      scriptsVendor: {
+        replace: ['build/**/*.html'],
+        replacement: 'vendor.js',
+        file: 'build/js/vendor.js'
+      }
+    },
+
+    clean: {
+      build: {
+        src: ['build/**/*']
+      }
+    },
+
+    connect: {
+      server: {
+        options: {
+          livereload: true,
+          base: 'build',
+          open: {
+            appName: 'Google Chrome'
+          }
+        }
+      }
+    },
+
+    concat: {
+      options: {
+        separator: '\n;\n'
+      },
+      vendor: {
+        src: [
+          // jQuery
+          'bower_components/jquery/dist/jquery.js',
+
+          // Twitter Bootstrap scripts, commenting out the unused features
+          'bower_components/bootstrap/js/transition.js',
+          'bower_components/bootstrap/js/alert.js',
+          'bower_components/bootstrap/js/button.js',
+          'bower_components/bootstrap/js/carousel.js',
+          'bower_components/bootstrap/js/collapse.js',
+          'bower_components/bootstrap/js/dropdown.js',
+          'bower_components/bootstrap/js/modal.js',
+          'bower_components/bootstrap/js/tooltip.js',
+          'bower_components/bootstrap/js/popover.js',
+          'bower_components/bootstrap/js/scrollspy.js',
+          'bower_components/bootstrap/js/tab.js',
+          'bower_components/bootstrap/js/affix.js'
+        ],
+        dest: 'build/js/vendor.js',
+      },
+    },
+
+    copy: {
+      html: {
+        files: [
+          {expand: true, src: ['index.html'], dest: 'build'}
+        ]
+      },
+      fonts: {
+        files: [
+          {expand: true, flatten: true, src: ['bower_components/bootstrap/fonts/*.*'], dest: 'build/fonts'}
+        ]
+      },
+      images: {
+        files: [
+          {expand: true, src: ['images/*.*'], dest: 'build'}
+        ]
+      },
+      scripts: {
+        files: [
+          {expand: true, src: ['js/bm.js'], dest: 'build'}
+        ]
+      }
+    },
+
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'images/',
+          src: ['*.*'],
+          dest: 'images/'
+        }]
+      }
+    },
+
+    jshint: {
+      options: {
+        reporter: require('jshint-stylish'),
+        browser: true,
+        curly: true,
+        eqnull: true,
+        eqeqeq: true,
+        undef: true,
+        devel: true,
+        ignores: ['js/vendor/*.js'],
+        globals: { }
+      },
+      files: {
+        src: ['js/**/*.js']
+      }
+    },
+
+    less: {
+      development: {
+        src: 'less/bm.less',
+        dest: 'build/css/bm.css'
+      },
+      production: {
+        options: {
+          cleancss: true
+        },
+        src: 'less/bm.less',
+        dest: 'build/css/bm.css'
+      }
+    },
+
+    uglify: {
+      options: {
+        report: 'min'
+      },
+      app: {
+        src: 'build/js/bm.js',
+        dest: 'build/js/bm.js'
+      },
+      vendor: {
+        src: 'build/js/vendor.js',
+        dest: 'build/js/vendor.js'
+      }
+    },
+
+    watch: {
+      options: {
+        livereload: true,
+      },
+      css: {
+        files: 'less/**/*.less',
+        tasks: ['less'],
+        options: {
+          spawn: false
+        }
+      },
+      html: {
+        files: ['*.html'],
+        tasks: ['copy:html'],
+        options: {
+          spawn: false
+        }
+      },
+      html: {
+        files: ['images/*.*'],
+        tasks: ['copy:images'],
+        options: {
+          spawn: false
+        }
+      },
+      scripts: {
+        files: 'js/**/*.js',
+        tasks: ['jshint', 'copy:scripts'],
+        options: {
+          spawn: false
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-cache-busting');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['clean:build', 'less:development', 'jshint', 'concat', 'copy', 'connect', 'watch']);
+  grunt.registerTask('build', ['clean:build', 'less:production', 'concat', 'imagemin', 'copy', 'uglify', 'cache-busting']);
+};
--- README.md
+++ README.md
@@ -1,31 +1,3 @@
 # The Butcher's Market
 
 Source code for The Butcher's Market website ([http://thebutchersmarket.com](http://thebutchersmarket.com)).
-
-# Things to do
-
-Here's a list of things that still need to be worked on:
-
-* Navbar - Font characteristics (Change to Bold), slightly less padding inbetween links, "down arrow" adjacent to "MORE"
-
-* Carousel - Make mobile display a seperate set of images. Reasons - Appearance, smaller file size
-
-* Social Media - Create small logos to float on the top left (position debatable) of the carousel
-
-* Images - 5 carousel images selected from the below to visually summarize the store for "index.html"
-
-         - 5 carousel images for "meat.html"
-
-         - 5 carousel images for "deli.html"
-
-         - 5 carousel images for "grocery.html"
-
-         - 5 carousel images for "dining.html"
-
-         - 5 carousel images for "beverage.html"
-
-         - A background image needs to be taken for "contact.html"
-
-
-
-
