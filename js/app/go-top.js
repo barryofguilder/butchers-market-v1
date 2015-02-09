9631065134df25f5b8a1973c58b24e405f1638b9
Jason Barry <barryofguilder@gmail.com>
2015-02-08 22:33:56 -0500

    Fix 'back to top' button so it doesn't show immediately for mobile

--- js/app/go-top.js
+++ js/app/go-top.js
@@ -12,17 +12,17 @@ butcher.goTopButton = function ($) {
 
     // Show or hide the sticky footer button
     $(window).scroll(function() {
-      if ($('.secondary-navigation').visible()) {
+      if ($('.secondary-navigation').visible() || $(this).scrollTop() < 550) {
         $('.go-top').fadeOut(200);
       } else {
         $('.go-top').fadeIn(200);
       }
     });
-    
+
     // Animate the scroll to top
     $('.go-top').click(function(e) {
       e.preventDefault();
-      
+
       $('html, body').animate({scrollTop: 0}, 300);
     });
 
